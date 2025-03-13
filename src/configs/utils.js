1ccdd0f6b96c59ccb07d784ab6f584ff3f0b6838
Angel <alu.angel.algemesi@gmail.com>
2025-03-13 14:02:12 +0100

    Check if username is in database

--- src/configs/utils.js
+++ src/configs/utils.js
@@ -2,3 +2,7 @@
 export const PAGE_LOGIN = 'Login' // screens/screenLogin/ResetPasswordScreen.js
 export const PAGE_RESET_PASSWORD = 'Reset' // screens/screenLogin/LoginScreen.js
 export const PAGE_REGISTER = 'Register' // screens/screenLogin/RegisterScreen.js
+
+
+//DATA BASE
+export const TABLE_USER = 'User'
--- src/screens/screenLogin/RegisterScreen.js
+++ src/screens/screenLogin/RegisterScreen.js
@@ -2,7 +2,7 @@ import { Text, View, Button,Alert } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useNavigation, } from '@react-navigation/native';
 import { TextInputLogin, CustomButtonOne } from 'components';
-import { PAGE_LOGIN, PAGE_RESET_PASSWORD, StringConstants } from 'configs';
+import { PAGE_LOGIN, PAGE_RESET_PASSWORD, StringConstants, TABLE_USER } from 'configs';
 import { StyleLogin } from 'styles';
 import { useState,useContext } from 'react';
 import { ThemeContext } from 'context';
@@ -20,6 +20,7 @@ const RegisterScreen = ({route}) => {
 
     // inputs y datos de Supabase y comunicar con hijos
     const [mailValue, setMailValue] = useState('');
+    const [usernameValue, setUsernameValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
     const [passwordConfirmationValue, setPasswordConfirmationValue] = useState('');
     const [loading, setLoading] = useState(false)
@@ -41,6 +42,23 @@ const RegisterScreen = ({route}) => {
 
     async function signUpWithEmail() {
         setLoading(true)
+
+
+        
+        //Check if the username doesn't exist in database
+        const { data, errorGet } = await supabase
+        .from(TABLE_USER)
+        .select('username')
+        .eq('username',usernameValue)
+
+        
+        if (Object.keys(data).length!=0){
+            console.log("username existente")
+        }else{
+            console.log("username correcto")
+        }
+
+
         const {
           data: { session },
           error,
@@ -63,9 +81,10 @@ const RegisterScreen = ({route}) => {
             .from('User')
             .insert([
                 {
-                    id:user.id,username:"Manolo",email:mailValue,name:"ManoloNombre",surname:"Gomez",password:passwordValue,created_at:date,last_login:date
+                    id:user.id,username:"pepe",email:mailValue,name:"ManoloNombre",surname:"Gomez",password:passwordValue,created_at:date,last_login:date
                 }
-            ])
+            ]).select()
+
             if (error) {
                 console.error('Error inserting user data:', error);
               } else {
@@ -90,6 +109,14 @@ const RegisterScreen = ({route}) => {
                 value={mailValue}
                 onChange={newText => setMailValue(newText)}
             />
+            <TextInputLogin
+                
+                placeholderText={t(StringConstants.username)}
+                
+                onChange={newText => setUsernameValue(newText)}
+            />
+
+
             <TextInputLogin
                 secureTextEntry
                 placeholderText={t(StringConstants.password)}
